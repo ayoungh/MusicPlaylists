753901dd1db7d810ef94a299b4f1e2f970d674d2
Anthony Young <github@ayoungh.co.uk>
2015-08-07 16:28:31 +0100

    Config file that will need filling in

---
+++ config.js
@@ -0,0 +1,13 @@
+//CONFIG
+
+//get your clientId, clientSecret details from https://developer.spotify.com/my-applications/#!/applications
+//username is your spotify username
+//You will need to make an authcode at https://developer.spotify.com/web-api/console/get-playlists/
+
+module.exports = {
+  clientId : '',
+  clientSecret : '',
+  redirectUri : 'http://localhost:3000/callback',
+  authCode : '',
+  username : ''
+};
